9e01647df3ee7459d7ccc692957da72bf1e6abe9
ndjerrou <n.djerrou@gmail.com>
2024-07-10 18:41:44 +0100

    App rdy for start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import MoviePlaylist from './components/MoviePlaylist';
+import SongPlaylist from './components/SongPlaylist';
 
-function App() {
-  return <div></div>;
+export default function App() {
+  const handleResetClick = () => {
+    //
+  };
+
+  return (
+    <div className='container is-fluid'>
+      <button onClick={() => handleResetClick()} className='button is-danger'>
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
+    </div>
+  );
 }
-
-export default App;
